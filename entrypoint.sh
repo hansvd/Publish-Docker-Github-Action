27e0c67eef553c4bd87453e04309e12ad6bdfcbe
Hans van Dodewaard <hans@s104.nl>
2020-01-01 13:40:41 +0100

    add working-directory input argument

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,8 +32,8 @@ echo ${INPUT_PASSWORD} | docker login -u ${INPUT_USERNAME} --password-stdin ${IN
 DOCKERNAME="${INPUT_NAME}:${BRANCH}"
 BUILDPARAMS="${INPUT_EXTRA_BUILD_PARAMS}"
 
-if [ ! -z "${INPUT_WORKINGDIR}" ]; then
-  cd ${INPUT_WORKINGDIR}
+if [ ! -z "${INPUT_WORKING-DIRECTORY}" ]; then
+  cd ${INPUT_WORKING-DIRECTORY}
 fi
 
 if [ ! -z "${INPUT_DOCKERNAME_APPENDIX}" ]; then
